8fd64def595cc83ff1170255df7517a8c2a831f7
Nathalia Cavalcanti <68089638+nathalia-cavalcanti@users.noreply.github.com>
2020-11-26 13:53:12 -0300

    Update contactform.php

--- forms/contactform.php
+++ forms/contactform.php
@@ -8,7 +8,7 @@ if (isset($_POST['submit'])) {
      $message = $_POST['message'];
      $subject= $_POST['subject'];
 
-     $mailTo="nathcvcanti@gmail.com";
+     $mailTo="email@email.com";
      $headers="From: ".$mailFrom;
 	 //$subject="Apoiadores Cloud Girls";
      $txt="Nome:" .$name. ".\nTel:" .$phone. ".\n\n".$message;
